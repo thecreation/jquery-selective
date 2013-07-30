28e68e20bf5c56f4fb4a07282d98097316900b2a
VicWei <VicWei1030@gmail.com>
2013-07-30 11:49:05 +0800

    first commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# jQuery Selective
+
+A jquery plugin that used to tagging, selectors and so on.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/amazingSurge/jquery-selective/master/dist/jquery-selective.min.js
+[max]: https://raw.github.com/amazingSurge/jquery-selective/master/dist/jquery-selective.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/jquery-selective.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ dist/jquery-selective.js
@@ -0,0 +1,43 @@
+/*! jQuery Selective - v0.1.0 - 2013-07-30
+* https://github.com/amazingSurge/jquery-selective
+* Copyright (c) 2013 amazingSurge; Licensed GPL */
+(function(window, document, $, undefined) {
+  "use strict";
+
+  // Constructor
+  var Selective = $.Selective = function(element, options) {
+    this.el = element;
+    this.$el = $(element);
+
+    this.options = $.extend(true, {}, Selective.defaults, options);
+  };
+
+  Selective.defaults = {
+
+  };
+
+  Selective.prototype = {
+
+  };
+
+  // Collection method.
+  $.fn.selective = function(options) {
+    if (typeof options === 'string') {
+      var method = options;
+      var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+
+      return this.each(function() {
+        var api = $.data(this, 'selective');
+        if (typeof api[method] === 'function') {
+          api[method].apply(api, method_arguments);
+        }
+      });
+    } else {
+      return this.each(function() {
+        if (!$.data(this, 'selective')) {
+          $.data(this, 'selective', new Selective(this, options));
+        }
+      });
+    }
+  };
+}(window, document, jQuery));
